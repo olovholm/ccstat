0aead24e8f30fa252edf3a408f886c49c215f358
Ola Løvholm <ola.lovholm@imv.uio.no>
2013-10-11 15:36:49 +0200

    old regime

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,19 +1,11 @@
 $(function () {
-<<<<<<< HEAD
 	  var chart1;
     var chart2;
     
-    
     $(document).ready(function() {
       scrollspyer();
-=======
-	var chart1;
+		var chart1;
     var chart2;
-	/* MAIN JS FILE DOCUMENT READY FUNCTION*/
-    $(document).ready(function() {
-      scrollspyer();
-	chart1;
->>>>>>> f4e2f235779371292a602297f3e8d0b72a1d811f
       
 
       /* Ready the flexslider */
@@ -21,7 +13,6 @@ $(function () {
 	  animation: "slide",
 	  controlsContainer: ".flex-container"
 	  });
-<<<<<<< HEAD
 
   });
 });
@@ -152,136 +143,6 @@ ay.pie_chart('which_devices',
 				} );
 				
 				graph2.render();
-=======
-	
-		/* Chart one - the platform distribution */
-		
-		var w = 500;
-		var h = 100;
-		var barPadding = 1; 
-		var dataset;
-		
-		
-	
-		
-		d3.json("data/barchart.json", function(data) {
-			dataset = data;
-			console.log(data);
-			drawbarchart();
-		});
-		function drawbarchart() {
-			var svg = d3.select("#cont1")
-			.append("svg")
-			.attr("height",h)
-			.attr("width",w);
-		
-		
-			svg.selectAll("rect")
-			.data(dataset)
-			.enter()
-			.append("rect")
-			.attr("x",function(d,i) {
-				return i * (w / dataset.length);
-			})
-			.attr("y", function(d){
-				 return h - d[1] *4;
-			})
-			.attr("width", w/ dataset.length - barPadding)
-			.attr("height",function(d){
-				return d[1] *4;
-			})
-			.attr("fill", function(d) {
-				console.log(d)
-				return "rgb(0,0,"+ (Math.round(d[1]) * 10) + ")";
-			})
-		};		
-		
-		
-	
-	
-	
-	 chart2 = new Highcharts.Chart({
-            chart: {
-                renderTo: 'cont2',
-                type: 'column',
-                margin: [ 50, 50, 100, 80]
-            },
-            title: {
-                text: 'World\'s largest cities per 2008'
-            },
-            xAxis: {
-                categories: [
-                    'Tokyo',
-                    'Jakarta',
-                    'New York',
-                    'Seoul',
-                    'Manila',
-                    'Mumbai',
-                    'Sao Paulo',
-                    'Mexico City',
-                    'Dehli',
-                    'Osaka',
-                    'Cairo',
-                    'Kolkata',
-                    'Los Angeles',
-                    'Shanghai',
-                    'Moscow',
-                    'Beijing',
-                    'Buenos Aires',
-                    'Guangzhou',
-                    'Shenzhen',
-                    'Istanbul'
-                ],
-                labels: {
-                    rotation: -45,
-                    align: 'right',
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Verdana, sans-serif'
-                    }
-                }
-            },
-            yAxis: {
-                min: 0,
-                title: {
-                    text: 'Population (millions)'
-                }
-            },
-            legend: {
-                enabled: false
-            },
-            tooltip: {
-                formatter: function() {
-                    return '<b>'+ this.x +'</b><br/>'+
-                        'Population in 2008: '+ Highcharts.numberFormat(this.y, 1) +
-                        ' millions';
-                }
-            },
-            series: [{
-                name: 'Population',
-                data: [34.4, 21.8, 20.1, 20, 19.6, 19.5, 19.1, 18.4, 18,
-                    17.3, 16.8, 15, 14.7, 14.5, 13.3, 12.8, 12.4, 11.8,
-                    11.7, 11.2],
-                dataLabels: {
-                    enabled: true,
-                    rotation: -90,
-                    color: '#FFFFFF',
-                    align: 'right',
-                    x: 4,
-                    y: 10,
-                    style: {
-                        fontSize: '13px',
-                        fontFamily: 'Verdana, sans-serif'
-                    }
-                }
-            }]
-        });
-    });
-    
-});
-
-
->>>>>>> f4e2f235779371292a602297f3e8d0b72a1d811f
 
 $(window).scroll(function(){ 
 		var offset = 0;
