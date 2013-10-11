0a5d3dc551b436c3380954e728cbd6b9606494ac
Ola Løvholm <ola.lovholm@imv.uio.no>
2013-10-11 15:08:31 +0200

    new head

--- Gemfile
+++ Gemfile
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 source 'https://rubygems.org'
 gem 'sinatra'
+=======
+source "https://rubygems.org"
+ruby '2.0.0'
+gem "sinatra", "1.4.3"
+gem "nokogiri"
+>>>>>>> f4e2f235779371292a602297f3e8d0b72a1d811f
--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@
 #encoding: UTF-8
 
 require 'sinatra'
-
+require 'rubygems'
+require 'csv'
+require 'json'
 
 
 
@@ -15,3 +17,64 @@ configure(:development){ p "RUNNING IN DEVELOPMENT ENVIRONMENT" }
 get '/' do 
   erb :index
 end
+
+
+get '/form' do 
+  erb :form
+end
+
+get '/d3' do 
+  erb :d3	
+end
+
+get '/streamtime' do
+  
+end
+
+get '/helloworld' do 
+  erb :d3
+  
+end
+
+
+get '/data/*.json' do
+  # Set in the data files and the arguments used to retrieve them here. 
+  datasets = {
+    streampercent: "search_client_dist.csv",
+    searchpercent: "streams_client_dist.csv",
+    barchart: "random_bar_num.csv"
+  }
+  
+  
+  puts "SOMEONE ASKED FOR: #{params[:splat]}"
+  
+  
+  datasource = datasets[params[:splat][0].to_sym]
+  puts "DATASOURCE: #{datasource}"
+
+  begin  
+    file = File.open("assets/data/#{datasource}","r")
+    CSV.parse(file, {:col_sep => ';'}).to_json
+  rescue
+    status 404 # Not found
+  end
+end
+
+
+
+=begin
+get '/bilde02.png' do
+  puts "Test test work to route"
+  png = ChunkyPNG::Image.new(200,400, ChunkyPNG::Color::TRANSPARENT)
+  (1...200).each do |x|
+    (1...400).each do |y|
+      png[x,y] = ChunkyPNG::Color.rgba(10, 20, 30, 128) unless y % 2 == 0
+    end
+  end
+  response.headers['content_type'] = "application/octet-stream"
+  puts "Getting ready to return: #{png.to_blob}"
+  response.write(png.to_blob)
+  
+end
+
+=end
